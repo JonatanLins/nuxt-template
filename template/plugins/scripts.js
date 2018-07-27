fd437f3e1fcdb18999b16557e8d5e5c2f2b37122
Jonatan Pinheiro Lins <jonatanpinheiro@outlook.com>
2018-07-27 16:43:33 -0300

    Correções no sistema de componentes

--- template/plugins/scripts.js
+++ template/plugins/scripts.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 // Pega todos os componentes da pasta 'components' e os registra globalmente
-const files = require.context('@/components', false, /\.vue$/)
+const files = require.context('@/components', true, /\.(js|vue)$/)
 files.keys().forEach(key => {
   Vue.component(key.replace(/(^\.\/)|(\.(js|vue)$)/g, ''), files(key).default)
 })
